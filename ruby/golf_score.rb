c989f7e51491ca5226b540728a89227e97a6b435
古澤透 <furusawatooru@furusawatoorunonotobukkukonpyuta.local>
2024-01-07 11:13:06 +0900

    reflect changes for golf_score.rb about rubocop request

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,44 +1,44 @@
 rules = gets.chomp.split(',')
 players = gets.chomp.split(',')
 
-for number in 0..17 do 
+for number in 0..17 do
   difference = rules[number].to_i - players[number].to_i
 
   if rules[number].to_i == 5
     if players[number].to_i == 2
-      score = "アルバトロス"
+      score = 'アルバトロス'
     elsif players[number].to_i == 1
-      score = "コンドル"
+      score = 'コンドル'
     elsif difference == -1
-      score = "ボギー"
+      score = 'ボギー'
     elsif difference <= -2
-      score = "#{difference*(-1)}ボギー"
-    elsif difference == 0
-      score = "パー"
+      score = "#{difference * (-1)}ボギー"
+    elsif difference.zero?
+      score = 'パー'
     elsif difference == 1
-      score = "バーディ"
+      score = 'バーディ'
     elsif difference == 2
-      score = "イーグル"
+      score = 'イーグル'
     end
   else
     if players[number].to_i == 1
-      score = "ホールインワン"
+      score = 'ホールインワン'
     elsif difference == -1
-      score = "ボギー"
+      score = 'ボギー'
     elsif difference <= -2
-      score = "#{difference*(-1)}ボギー"
-    elsif difference == 0
-      score = "パー"
+      score = "#{difference * (-1)}ボギー"
+    elsif difference.zero?
+      score = 'パー'
     elsif difference == 1
-      score = "バーディ"
+      score = 'バーディ'
     elsif difference == 2
-      score = "イーグル"
+      score = 'イーグル'
     end
   end
 
   if number < 17
     print "#{score},"
-  elsif
+  else
     print "#{score}\n"
   end
   number += 1
